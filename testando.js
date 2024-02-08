8f8cd790b7bce1ce63b6be0428494d09e460c6f1
João Cherry <158841379+JoaoCherry@users.noreply.github.com>
2024-02-08 11:54:33 -0300

    Add files via upload

---
+++ testando.js
@@ -0,0 +1,13 @@
+var Nota1 = 10;
+var Nota2 = 10;
+var Total = ((Nota1 + Nota2) / 2);
+
+if (Total < 6){
+    console.log("Sua nota foi " , Total , ", infelizmente você não passou.")
+}else{
+    console.log("Sua nota foi " , Total , ", você passou!")
+}
+
+if (Total = 10){
+    console.log("Você arrasou!")
+}
